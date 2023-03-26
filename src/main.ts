3c61caf825c007c36c5a37645adcfe94b77edc96
Emilio P <ea.pugaavalos.99@gmail.com>
2023-03-26 01:58:06 -0600

    Agregar tailwind

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+import './styles/global.css'
 import vuetify from './plugins/vuetify'
 
 createApp(App)
---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: 'tw-',
+  corePlugins: {
+    preflight: false
+  },
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
