3768706739c20cbffde0c3a640e88ecb65b9f8c3
Emilio P <ea.pugaavalos.99@gmail.com>
2023-03-27 00:48:41 -0600

    Configurar eslint tailwind

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,26 @@
 /* eslint-disable no-undef */
 module.exports = {
   root: true,
+
+  plugins: ['tailwindcss'],
+
   extends: [
     'plugin:vue/vue3-recommended',
     'eslint:recommended',
-    '@vue/eslint-config-typescript'
+    '@vue/eslint-config-typescript',
+    'plugin:tailwindcss/recommended',
   ],
+  settings: {
+    'tailwindcss': {
+      'config': 'tailwind.config.cjs'
+    }
+  },
   parserOptions: {
     ecmaVersion: 'latest'
   },
   rules: {
     semi: ['error', 'never'],
-    'quotes': [2, 'single', { 'avoidEscape': true }]
+    'quotes': [2, 'single', { 'avoidEscape': true }],
+    'tailwindcss/no-custom-classname': 'off'
   }
 }
