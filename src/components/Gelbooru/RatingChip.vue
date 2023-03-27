ef14243ce715fcd55ce7522e0f7a60ee85fef3d6
Emilio P <ea.pugaavalos.99@gmail.com>
2023-03-26 23:24:56 -0600

    Agregar rating chip

---
+++ src/components/Gelbooru/RatingChip.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-chip
+    :color="color"
+    v-bind="$attrs"
+  >
+    <span class="tw-capitalize">
+      {{ propsRatingChip.rating }}
+    </span>
+  </v-chip>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+
+const propsRatingChip = defineProps({
+  rating: {
+    type: String,
+    default: ''
+  }
+})
+
+const color = computed(() => {
+  switch (propsRatingChip.rating) {
+    case 'safe':
+    case 'Safe':
+      return 'green'
+
+    case 'questionable':
+    case 'sensitive':
+    case 'Questionable':
+      return 'amber'
+
+    case 'explicit':
+    case 'Explicit':
+      return 'red'
+
+    default:
+      return 'gray'
+  }
+})
+
+
+</script>
+
+<style scoped>
+
+</style>
