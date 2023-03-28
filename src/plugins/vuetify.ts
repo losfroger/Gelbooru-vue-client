d279542600b0278b19b94205de6d75b4ea541cf8
Emilio P <ea.pugaavalos.99@gmail.com>
2023-03-27 23:32:01 -0600

    Cambiar colores del tema y agregar los colores a tailwind

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,11 +1,29 @@
 import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
+import { ThemeDefinition, createVuetify } from 'vuetify'
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
 import '@mdi/font/css/materialdesignicons.css'
 
+const darkTheme: ThemeDefinition = {
+  dark: true,
+  colors: {
+    background: '#121212',
+    error: '#CF6679',
+    info: '#2196F3',
+    primary: '#006FFA',
+    secondary: '#BB86FC',
+    accent: '#03DAC5',
+    success: '#4CAF50',
+    surface: '#212121',
+    warning: '#FB8C00',
+  }
+}
+
 const vuetify = createVuetify({
   theme: {
-    defaultTheme: 'dark'
+    defaultTheme: 'darkTheme',
+    themes: {
+      darkTheme
+    }
   },
   icons: {
     defaultSet: 'mdi',
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,7 +11,26 @@ module.exports = {
     './src/**/*.{vue,js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {},
+    screens: {
+      'xs': '0px',
+      'sm': '600px',
+      'md': '960px',
+      'lg': '1264px',
+      'xl': '1904px',
+    },
+    extend: {
+      colors: {
+        background: '#121212',
+        error: '#CF6679',
+        info: '#2196F3',
+        primary: '#006FFA',
+        secondary: '#BB86FC',
+        accent: '#03DAC5',
+        success: '#4CAF50',
+        surface: '#212121',
+        warning: '#FB8C00',
+      }
+    },
   },
   plugins: [],
 }
