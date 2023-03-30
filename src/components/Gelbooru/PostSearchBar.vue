a8a07cc7843a383d337e3bc476e918b3d00df406
Emilio P <ea.pugaavalos.99@gmail.com>
2023-03-29 19:56:03 -0600

    Cambios al diseño de la barra de búsqueda

--- src/components/Gelbooru/PostSearchBar.vue
+++ src/components/Gelbooru/PostSearchBar.vue
@@ -1,28 +1,47 @@
 <template>
-  <div class="search-bar tw-flex tw-flex-row tw-justify-center tw-gap-2">
-    <v-text-field
-      class="tw-w-4/5"
-      label="Search"
-      prepend-inner-icon="mdi-magnify"
-    />
-    <div class="tw-flex tw-w-1/5 tw-flex-row tw-gap-2">
-      <v-select
-        :items="select.items"
-        label="Sort by"
+  <div>
+    <div class="search-bar tw-flex tw-flex-row tw-justify-center tw-gap-2">
+      <v-text-field
+        class="tw-w-4/5"
+        label="Search"
+        prepend-inner-icon="mdi-magnify"
+        hide-details
+        color="primary"
       />
-      <v-btn
-        variant="text"
-        :class="{'tw-rotate-180': !select.desc}"
-        icon="mdi-arrow-down"
-        @click="select.desc = !select.desc"
-      />
+      <div class="tw-flex tw-w-1/5 tw-flex-row tw-items-center tw-gap-2">
+        <v-select
+          :items="select.items"
+          label="Sort by"
+          color="primary"
+          hide-details
+        />
+        <v-btn
+          variant="text"
+          :class="{'tw-rotate-180': !select.desc}"
+          icon="mdi-arrow-down"
+          @click="select.desc = !select.desc"
+        />
+      </div>
     </div>
+    <v-expand-transition>
+      <v-progress-linear
+        v-if="propsSearchBar.loading"
+        indeterminate
+        color="primary"
+      />
+    </v-expand-transition>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 
+const propsSearchBar = defineProps({
+  loading: {
+    type: Boolean,
+    default: false,
+  }
+})
 
 const select = ref({
   desc: true,
